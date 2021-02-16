9c297e47e67aa4e5b2b0bc4feef56d6541c481d8
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-02-16 02:49:08 +0100

    Create Fixture class to share context beween tests

---
+++ SolidCalculator.CoreTests/InitializeRepoFixture.cs
@@ -0,0 +1,18 @@
+﻿using SolidCalculator.Core;
+
+namespace SolidCalculator.CoreTests
+{
+    /// <summary>
+    /// A class to be used in Xunit IClassFixture interface for sharing context between tests
+    /// </summary>
+    public class InitializeRepoFixture
+    {
+        public IArithmeticOperationsRepo ArithmeticOperations { get; }
+
+        public InitializeRepoFixture()
+        {
+            ServicesConfig.Initialize();
+            ArithmeticOperations = ServicesConfig.OperationsRepo;  // passes in an instance of the ArithmeticOperationRepo
+        }
+    }
+}

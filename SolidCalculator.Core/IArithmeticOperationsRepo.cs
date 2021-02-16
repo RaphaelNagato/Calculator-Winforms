f95b629e9ecd680d96e4bc9fd7d4136fddeb2e36
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-02-16 02:48:03 +0100

    Create Interface for methods to be implemented

---
+++ SolidCalculator.Core/IArithmeticOperationsRepo.cs
@@ -0,0 +1,48 @@
+﻿using SolidCalculator.Commons;
+
+namespace SolidCalculator.Core
+{
+    public interface IArithmeticOperationsRepo
+    {
+        /// <summary>
+        /// Implements Addition operation between the two inputs
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>double</returns>
+        double Add(double firstNumber, double secondNumber);
+
+        /// <summary>
+        /// Implements Subtract or minus operation between the two inputs
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>double</returns>
+        double Subtract(double firstNumber, double secondNumber);
+
+        /// <summary>
+        /// Implements Multiply operation between two inputs
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>double</returns>
+        double Multiply(double firstNumber, double secondNumber);
+
+        /// <summary>
+        /// Implements Division operation between two inputs, the second input is the divisor
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>double</returns>
+        double Divide(double firstNumber, double secondNumber);
+
+        /// <summary>
+        /// maps the OperatorEnum to the methods performing the operation
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="sign"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>double</returns>
+        double Calc(double firstNumber, OperatorEnum sign, double secondNumber);
+    }
+}
